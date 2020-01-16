b84a1f5239da3ce646a003174535f797e715ac94
Phil <thegoldcrayon101@gmail.com>
2020-01-15 21:48:55 -0600

    Add flax seeds to grass drop, add string/dry flax recipe

---
+++ src/main/java/com/github/thegoldcrayon/tgcropesmod/EventHandler.java
@@ -0,0 +1,28 @@
+package com.github.thegoldcrayon.tgcropesmod;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootPool;
+import net.minecraft.world.storage.loot.TableLootEntry;
+import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod.EventBusSubscriber(modid = TGCRopesMod.MODID)
+public class EventHandler
+{
+    private static ResourceLocation grass = new ResourceLocation("minecraft", "blocks/grass");
+    private static ResourceLocation newTable = new ResourceLocation(TGCRopesMod.MODID, "blocks/grass");
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    @SubscribeEvent
+    public static void onLootLoad(LootTableLoadEvent event)
+    {
+        if(event.getName().equals(grass))
+            event.getTable().addPool(LootPool.builder().addEntry(TableLootEntry.builder(newTable)).build());
+
+        LOGGER.debug("Loaded loot table additions");
+    }
+}
