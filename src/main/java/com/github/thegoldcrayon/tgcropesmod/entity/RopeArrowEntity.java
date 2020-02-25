67174fc6ad9fb22d36fbfea790bf0842a660d49a
Phil <thegoldcrayon101@gmail.com>
2020-02-25 17:44:05 -0600

    change arrow recipe, make arrow spawn rope

--- src/main/java/com/github/thegoldcrayon/tgcropesmod/entity/RopeArrowEntity.java
+++ src/main/java/com/github/thegoldcrayon/tgcropesmod/entity/RopeArrowEntity.java
@@ -2,19 +2,28 @@ package com.github.thegoldcrayon.tgcropesmod.entity;
 
 import com.github.thegoldcrayon.tgcropesmod.init.ModRegistry;
 import com.github.thegoldcrayon.tgcropesmod.item.RopeArrow;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.IPacket;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.FMLPlayMessages;
 import net.minecraftforge.fml.network.NetworkHooks;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nonnull;
 
 public class RopeArrowEntity extends ArrowEntity
 {
+    public static final Logger LOGGER = LogManager.getLogger();
+
     public RopeArrowEntity(FMLPlayMessages.SpawnEntity spawnPacket, World world)
     {
         super(world, 0, 0, 0);
@@ -39,13 +48,39 @@ public class RopeArrowEntity extends ArrowEntity
     @Override
     public void tick()
     {
-        if(!world.isRemote)
+        /*if(!world.isRemote)
         {
             //once this no longer crashes, set up rope so that it checks the face of the block it lands on.
             // if valid, place rope all the way down until can't place more, or player runs out of rope in inventory.
             // must use rope from player's inv.
-            super.tick();
+        }*/
+        if(this.inGround)
+        {
+            BlockPos position = this.getPosition();
+            Block checkBlock = world.getBlockState(position.up()).getBlock();
+            int maxRopeUsable = 27;
+            if(!(checkBlock == Blocks.AIR) && world.getBlockState(position).getBlock() == Blocks.AIR)
+            {
+                int ropeUsed = 0;
+                Boolean isAir = true;
+                Block startingBlock = world.getBlockState(position).getBlock();
+                Block currentBlock = startingBlock;
+                BlockPos startingPos = position;
+                BlockPos currentPos = startingPos;
+
+                while(isAir && ropeUsed < maxRopeUsable)
+                {
+                    world.setBlockState(currentPos, ModRegistry.ROPE.get().getDefaultState());
+                    currentPos = currentPos.down();
+                    currentBlock = world.getBlockState(currentPos).getBlock();
+                    if(currentBlock != Blocks.AIR)
+                        isAir = false;
+                    ropeUsed++;
+                }
+                this.remove();
+            }
         }
+        super.tick();
     }
 
     @Override
